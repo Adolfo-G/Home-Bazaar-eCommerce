b64bb558272062828dc5a2fbc387a1803eb76ab5
Adolfo Gonzalez <agonzal177@gmail.com>
2022-05-18 17:18:04 -0400

    added graphql/auth setup in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
@@ -10,9 +8,30 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <h1>hello</h1>
+    </ApolloProvider>
   );
 }
 
